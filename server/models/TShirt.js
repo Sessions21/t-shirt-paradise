8a8932dfc5199343f595f035f59bc11d0abb7b94
tkcannon <taylorkcannon@gmail.com>
2022-03-31 15:10:24 -0600

    fixed errors preventing server start / commented out dateFormate ref

--- server/models/TShirt.js
+++ server/models/TShirt.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const dateFormat = require("../utils/dateFormat");
+// const dateFormat = require("../utils/dateFormat");
 
 const tShirtSchema = new Schema(
   {
@@ -24,44 +24,31 @@ const tShirtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      // get: (createdAtVal) => dateFormat(createdAtVal),
     },
     // image or image arr. Might work better with image as it's own schema
-    
+
     // category currently in it's own file might be able to just add it to tshirt
 
     // Use comment schema
-    comments: [CommentSchema],
-  }
-);
-
-const CommentSchema = new Schema(
-  {
-    writtenBy: {
-      type: String,
-      required: true,
-    },
-    commentBody: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
-    // use ReplySchema to validate data for a reply
-    // replies: [ReplySchema],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
   },
   {
     toJSON: {
       virtuals: true,
-      getters: true,
+      getters: true
     },
-    id: false,
   }
 );
+
+tShirtSchema.virtual('commentCount').get(() => {
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
 
 const TShirt = mongoose.model('TShirt', tShirtSchema);
 
