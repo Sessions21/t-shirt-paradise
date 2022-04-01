636e1183e73848e96a108fa3b3c818f1d65a4ec7
Scott Nichols <s21nichols@hotmail.com>
2022-04-01 17:05:55 -0600

    finished homepage styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,14 @@ import React from 'react';
 import coverImage from "../assets/images/cover-2.png"
 
 function Home() {
+
   return (
-    <div className='showcase'>
+    <div>
       <img className="background" src={coverImage} alt="paradise scene"></img>
+      <div className='start'>
+        <h2 className='intro'>The place to share your <span className='love'>LOVE</span> of the T-Shirt!</h2>
+        <a href="/login"><button className='startButton'> Get Started </button></a>
+      </div>
     </div>
   )
 }
