cc812ad062040a30ee93e83e6e5c8d606c174fff
tkcannon <taylorkcannon@gmail.com>
2022-04-05 08:11:15 -0600

    added more filler seed data

--- server/models/User.js
+++ server/models/User.js
@@ -2,30 +2,38 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6
+    },
+    tshirts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'TShirt'
+      }
+    ]
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  tshirts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'TShirt'
-    }
-  ]
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+  }
+);
 
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
