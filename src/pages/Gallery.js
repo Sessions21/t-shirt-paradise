f77f0380af526e58d866cb438ab10f6cddccc550
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-04-04 23:13:55 -0600

    map over tshirtlist and display

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import TshirtList from "../components/TshirtList";
 
-
 const Gallery = () => {
-
   // const trialUsers = fakeData;
-  const [Tshirt, setTshirt] = useState([]);
+  const [Tshirt, setTshirt] = useState([TshirtList]);
+
   return (
     <div className="Gallery container">
-      <h1 className="gallery title"> T-Shirt Paradise User Submitted Gallery </h1>
-
-      <TshirtList />
+      <h1 className="gallery title">
+        T-Shirt Paradise User Submitted Gallery
+      </h1>
+      <div className="tshirt-container">
+        {Tshirt.map(<TshirtList></TshirtList>)}
+      </div>
     </div>
   );
 };
