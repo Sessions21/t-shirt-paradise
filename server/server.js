6038e8b29f12245fd2e833ffd6f9f992e86fed5f
tkcannon <taylorkcannon@gmail.com>
2022-04-07 23:35:01 -0600

    changed production from ../client/build to ../build

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, '../build')))
 }
 
 db.once('open', () => {
