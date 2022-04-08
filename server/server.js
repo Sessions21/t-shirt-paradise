bbc01a6c4db11677579c218b544d550356f33ab3
tkcannon <taylorkcannon@gmail.com>
2022-04-07 23:41:22 -0600

    another path change

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../build')))
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
